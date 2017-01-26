850758bff88a867f7b58332c744be699843c7a02
Galen Strasen <galen@wearetmbr.com>
2017-01-25 23:58:45 -0800

    whitespace

--- wp-content/themes/darkcity/front-page.php
+++ wp-content/themes/darkcity/front-page.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>	
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <div id="page-wrap">
 
@@ -7,32 +7,32 @@
 		<div class="anchor" id="about-anchor"></div>
 		<?php get_template_part('partials/about'); ?>
 	</div><!-- /relative -->
-	
+
 	<div class="relative">
 		<div class="anchor" id="rates-anchor"></div>
-		<?php get_template_part('partials/rates'); ?>	
+		<?php get_template_part('partials/rates'); ?>
 	</div><!-- /relative -->
-		
+
 	<div class="relative">
 		<div class="anchor" id="photos-anchor"></div>
 		<?php get_template_part('partials/photos'); ?>
 	</div><!-- /relative -->
-	
+
 	<div class="relative">
 		<div class="anchor" id="equipment-anchor"></div>
 		<?php get_template_part('partials/equipment'); ?>
 	</div><!-- /relative -->
-	
+
 	<div class="relative">
 		<div class="anchor" id="projects-anchor"></div>
 		<?php get_template_part('partials/projects'); ?>
 	</div><!-- /relative -->
-	
+
 	<div class="relative">
 		<div class="anchor" id="contact-anchor"></div>
 		<?php get_template_part('partials/contact'); ?>
 	</div><!-- /relative -->
-	
+
 </div><!-- #page-wrap -->
 
 <?php endwhile; endif; ?>
