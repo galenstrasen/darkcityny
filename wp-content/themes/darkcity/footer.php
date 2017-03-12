915510da5b87f0404832bc07a191142ab4bf5446
Galen Strasen <galen@wearetmbr.com>
2017-03-12 16:50:22 -0700

    Footer style and markup

--- wp-content/themes/darkcity/footer.php
+++ wp-content/themes/darkcity/footer.php
@@ -1,16 +1,21 @@
-<div id="footer">
-	
-	
+<footer id="footer">
 
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-sm-12">
-				<p class="privacy"><?php echo sprintf( __( '%1$s %2$s'), '&copy; Copyright', date('Y'));  ?>, Dark City Studios. All Rights Reserved.</p>
+			<div class="col-xs-12 col-sm-4">
+				<ul class="social">
+					<li><a href="https://www.facebook.com/MassDestructionEnt/" class="social-icon" target="_blank"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="https://www.instagram.com/darkcitystudios/" class="social-icon" target="_blank"><i class="fa fa-instagram"></i></a></li>
+				</ul>
+			</div>
+			<div class="col-xs-12 col-sm-8">
+				<p class="privacy"><?php echo sprintf( __( '%1$s %2$s'), '&copy; Copyright', date('Y'));  ?>, Dark City Studios. All Rights Reserved. <a href="http://shespeaksincode.com" class="galen" target="_blank" title="Galen Strasen | Web Developer | She Speaks in Code">Website Development by SSIC.</a></p>
 			</div><!-- /col -->
+
 		</div><!-- /row -->
 	</div><!-- /container -->
-	
-</div><!-- #footer -->
+
+</footer><!-- #footer -->
 
 
 
