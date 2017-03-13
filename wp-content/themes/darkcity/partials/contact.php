ddc4685c8239c29038329450646568731db33f21
Galen Strasen <galen@wearetmbr.com>
2017-03-12 18:19:37 -0700

    Local business schema

--- wp-content/themes/darkcity/partials/contact.php
+++ wp-content/themes/darkcity/partials/contact.php
@@ -4,22 +4,39 @@
 	<div class="container-fluid no-gutter white">
 		<div class="row">
 			<div class="col-md-5 col-md-offset-2 col-sm-6">
-				
 				<h3 class="h1">Contact Us</h3>
-				<p>
-				Dark City Studios<br/>
-				508 Nepperhan Ave. <br/>
-				3rd Floor<br/>
-				Yonkers, NY 10701
-				</p>
-			
-				<p>
-				<i class="fa fa-envelope-o"></i> <a href="mailto:andy@darkcityny.com">andy@darkcityny.com</a><br/>
-				<i class="fa fa-phone"></i> (646)623.2313
-				</p>
-				
+				<div itemscope itemtype="http://schema.org/LocalBusiness">
+					<p>
+						<strong><span itemprop="name">Dark City Studios</span></strong><br/>
+						<span itemprop="description">Recording Studio in Westchester, NY</span>
+					</p>
+
+					<p>
+						<span itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+							<span itemprop="streetAddress">
+								508 Nepperhan Ave. <br/>
+								3rd Floor
+							</span><br/>
+							<span itemprop="addressLocality">Yonkers</span>,
+							<span itemprop="addressRegion">New York</span>
+							<span itemprop="postalCode">10701</span>
+						</span>
+					</p>
+
+					<p>
+						<i class="fa fa-envelope-o"></i>
+						<a href="mailto:andy@darkcityny.com">
+							<span itemprop="email">andy@darkcityny.com</span>
+						</a>
+
+						<br/>
+
+						<i class="fa fa-phone"></i>
+						<span itemprop="telephone">(646)623.2313</span>
+					</p>
+				</div><!-- /itemscope -->
 			</div><!-- /col -->
-			
+
 			<div class="col-md-5 col-sm-6">
 				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12054.941792538255!2d-73.8837456!3d40.9434649!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2ed7eb2c2df73%3A0xe2d5087fb84d24f4!2s508+Nepperhan+Ave%2C+Yonkers%2C+NY+10701!5e0!3m2!1sen!2sus!4v1416178502964" style="max-width:100%!important;" width="800" height="400" frameborder="0" style="border:0"></iframe>
 			</div>
