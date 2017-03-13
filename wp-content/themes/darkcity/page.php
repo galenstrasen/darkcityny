5d7323de683a230e6e6e5d6c28bdc7d967f307b4
Galen Strasen <galen@wearetmbr.com>
2017-03-12 19:05:17 -0700

    page markup

--- wp-content/themes/darkcity/page.php
+++ wp-content/themes/darkcity/page.php
@@ -1,18 +1,41 @@
 <?php get_header(); ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-  <main id="page-wrap" class="page-content basic">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-lg-8 col-lg-offset-2">
-          <article class="main-content">
-          <h1 class="page-title"><?php the_title(); ?></h1>
-					<?php the_content(); ?>
-          </article>
-				</div>
-      </div>
-    </div>
-  </main>
-<?php endwhile; endif; ?>
-<?php //edit_post_link();
 
-get_footer(); ?>
+<main id="page-wrap" class="page-content basic">
+
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-8 col-lg-offset-2">
+        <article class="main-content">
+          <h1 class="page-title"><?php the_title(); ?></h1>
+				  <?php the_content(); ?>
+        </article>
+			</div>
+    </div>
+  </div>
+  <?php endwhile; endif; ?>
+  <div class="relative animation" data-animation="animation-fade-in-down">
+    <div class="anchor" id="rates-anchor"></div>
+    <?php get_template_part('partials/rates'); ?>
+  </div><!-- /relative -->
+
+  <div class="relative animation" data-animation="animation-fade-in-down">
+    <div class="anchor" id="photos-anchor"></div>
+    <?php get_template_part('partials/photos'); ?>
+  </div><!-- /relative -->
+
+  <div class="relative animation" data-animation="animation-fade-in-down">
+    <div class="anchor" id="equipment-anchor"></div>
+    <?php get_template_part('partials/equipment'); ?>
+  </div><!-- /relative -->
+
+  <div class="relative animation" data-animation="animation-fade-in-down">
+    <div class="anchor" id="contact-anchor"></div>
+    <?php get_template_part('partials/contact'); ?>
+  </div><!-- /relative -->
+
+</main>
+
+
+<?php
+get_footer();
