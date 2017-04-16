3a3804d158f871a55c0d3803c1d4cd57592a8f43
Galen Strasen <galen@wearetmbr.com>
2017-04-16 15:39:20 -0700

    Add title tag

--- wp-content/themes/darkcity/header.php
+++ wp-content/themes/darkcity/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	<title><?php wp_title(''); ?></title>
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="shortcut icon" href="<?php bloginfo( 'template_url' ); ?>/favicon.ico">
